01e6a3748ab421d05333c6b80c1a868cd578872c
Umair Iqbal <ui0886342@gmail.com>
2026-06-21 14:27:46 +0500

    Deleted unnecessary files and merged some into others

--- src/components/footer/LinksFooter.jsx
+++
@@ -1,8 +0,0 @@
-
-function LinksFooter({ link, icon }) {
-  return (
-    <a className="inline-flex bg-heading text-white text-[16px] lg:text-[18px] p-1 lg:p-[6.4px] rounded-sm lg:rounded-lg hover:bg-heading-dark" href={link} target="_blank">{icon}</a>
-  )
-}
-
-export default LinksFooter
